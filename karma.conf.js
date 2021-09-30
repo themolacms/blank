c6819b3b6cfce1b32a505117ad25d416c00d990a
Lam Nhan <lamnhan92@gmail.com>
2021-09-30 20:15:42 +0700

    update deps and rename project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/blank'),
+      dir: require('path').join(__dirname, './coverage/blank-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
